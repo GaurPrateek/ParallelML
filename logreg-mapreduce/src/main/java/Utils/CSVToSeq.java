37d9e7e4de046bbf87d4a465ca90c7480320e9c2
Prateek Gaur <pgaur19@gmail.com>
2014-05-04 18:54:59 +0200

    Improved Directory Structure

---
+++ logreg-mapreduce/src/main/java/Utils/CSVToSeq.java
@@ -0,0 +1,138 @@
+package Utils;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.mahout.common.Pair;
+import org.apache.mahout.common.iterator.sequencefile.SequenceFileIterable;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Closeables;
+
+import de.tuberlin.dima.ml.util.CsvReader;
+import de.tuberlin.dima.ml.util.MLUtils;
+
+public class CSVToSeq {
+  
+  /**
+   * Stores result also as text file (csv), relevant if data were normalized
+   */
+  public static void transform(
+      String sourcePath,
+      String targetPath,
+      String targetCsvPath,
+      int rows, 
+      List<String> predictorNames, 
+      String targetName,
+      boolean normalizeData,
+      boolean normalizeLabels,
+      double targetPositive, 
+      double targetNegative) throws Exception {
+    
+    Configuration conf = new Configuration();
+    FileSystem fs = FileSystem.getLocal(conf);
+    SequenceFile.Writer writer = null;
+    Writer csvWriter = new BufferedWriter(new OutputStreamWriter(
+        new FileOutputStream(targetCsvPath), "UTF-8"));
+  
+    try {
+      CsvReader csv = MLUtils.readData(sourcePath, rows, predictorNames, targetName, false);
+      if (normalizeData){
+        csv.normalize();
+      }
+      if (normalizeLabels) {
+        csv.normalizeClassLabels(targetPositive, targetNegative);
+      }
+      
+      writer = SequenceFile.createWriter(fs, conf, new Path(targetPath),
+          IntWritable.class, VectorWritable.class);
+      
+      IntWritable label = new IntWritable();
+      VectorWritable vector = new VectorWritable();
+      int i=0;
+      for (; i< csv.getData().numRows(); ++i) {
+        label.set((int)csv.getY().get(i));
+        vector.set(csv.getData().viewRow(i));
+        writer.append(label, vector);
+        
+        for (int col=0; col<vector.get().size(); ++col) {
+          if (col>0) csvWriter.write(",");
+          csvWriter.write(Double.toString(vector.get().get(col)));
+        }
+        csvWriter.write("\n");
+      }
+      System.out.println("Wrote " + i  + " records into sequence file " + targetPath);
+    } finally {
+      Closeables.close(writer, true);
+      Closeables.close(csvWriter, true);
+    }
+  }
+  
+  public static void main(String[] args) throws Exception {
+
+    List<String> predictorNames = Lists.newArrayList(new String[] {
+        "x", "y", "shape", "xx", "xy", "yy", "a", "b", "c"     // All relevant fields which are in both, training and test file
+     });
+    
+    String testFile = "donut-test.csv";
+    String trainingFile = "donut.csv";
+    int rows = 40;
+    String targetName = "color";
+    double targetPositive = 2d;
+    double targetNegative = 1d;
+    
+    CSVToSeq.transform(
+        testFile, 
+        testFile.concat(".seq"), 
+        testFile.concat(".normalized"), 
+        rows, 
+        predictorNames,
+        targetName, 
+        true,
+        true,
+        targetPositive, 
+        targetNegative);
+    
+    CSVToSeq.transform(
+        trainingFile,
+        trainingFile.concat(".seq"),
+        trainingFile.concat(".normalized"),
+        rows,
+        predictorNames,
+        targetName,
+        true,
+        true,
+        targetPositive, 
+        targetNegative);
+    
+    Configuration conf = new Configuration();
+
+    System.out.println("Print first records out of sequence file:");
+    
+    int n = 0;
+    for (Pair<IntWritable, VectorWritable> labeledRecord : 
+        new SequenceFileIterable<IntWritable, VectorWritable>(new Path(testFile.concat(".seq")), conf)) {
+
+      System.out.println("Label: " + labeledRecord.getFirst().get());
+
+      Vector features = labeledRecord.getSecond().get();
+
+      System.out.println("- Features: " + features.getNumNondefaultElements() + " of " + features.size());
+      System.out.println("- Vec: " + features.toString());
+
+      if (++n == 2) {
+        break;
+      }
+    }
+  }
+}
---
+++ logreg-mapreduce/src/main/java/mapredBatchLogreg/BatchGradientTest.java
@@ -0,0 +1,45 @@
+package mapredBatchLogreg;
+
+import Utils.RCV1DatasetInfo;
+import de.tuberlin.dima.ml.datasets.DatasetInfo;
+
+
+
+public class BatchGradientTest {
+  
+  private static final DatasetInfo DATASET = RCV1DatasetInfo.get();
+  
+  private static final String TARGET_POSITIVE = "CCAT";
+
+//  private static final String INPUT_FILE_TRAIN = "/Users/uce/Desktop/rcv1-v2/vectors/lyrl2004_vectors_train_5000.seq";
+//  private static final String INPUT_FILE_TRAIN ="hdfs://localhost:9000/user/uce/rcv1/rcv1-v2/lyrl2004_vectors_train_5000.seq";
+  private static final String INPUT_FILE_TRAIN = "/Users/prateekgaur/Downloads/sequencefiles/lyrl2004_vectors_train_2000.seq";
+//  private static final String INPUT_FILE_TRAIN ="datasets/rcv1-v2/lyrl2004_vectors_train_5000.seq";
+  
+  private static final String OUTPUT_TRAIN_PATH = "output-aim3-batchgd";
+  
+  private static final int MAX_ITERATIONS = 3;
+
+  public static void main(String[] args) throws Exception {
+    
+    int labelDimension = DATASET.getLabelIdByName(TARGET_POSITIVE);
+
+    double initial = 1;
+    
+    String hdfsAddress = "";
+    String jobtrackerAddress = "";
+    
+    BatchGradientDriver bgDriver = new BatchGradientDriver(
+        INPUT_FILE_TRAIN, 
+        OUTPUT_TRAIN_PATH, 
+        MAX_ITERATIONS,
+        initial,
+        labelDimension,
+        (int)DATASET.getNumFeatures(),
+        hdfsAddress,
+        jobtrackerAddress);
+    
+    bgDriver.train();
+  }
+  
+}
