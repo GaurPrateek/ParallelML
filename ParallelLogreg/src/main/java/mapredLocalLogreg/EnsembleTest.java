300ca16e5c3df0830b180e73d493a7836aed059c
Prateek Gaur <pgaur19@gmail.com>
2014-05-10 01:46:13 +0200

    basic refactoring of logreg package

--- ParallelLogreg/src/main/java/mapredLocalLogreg/EnsembleTest.java
+++ ParallelLogreg/src/main/java/mapredLocalLogreg/EnsembleTest.java
@@ -7,40 +7,40 @@ import de.tuberlin.dima.ml.datasets.DatasetInfo;
 
 
 public class EnsembleTest {
-  
-  private static final DatasetInfo DATASET = RCV1DatasetInfo.get();
-  
-  private static final int ENSEMBLE_SIZE = 4;
-  
-  // Currently we train a hardcoded single 1-vs-all classifier
-  private static final String TARGET_POSITIVE = "CCAT";
-  
-  private static final String INPUT_FILE_TRAIN = "/Users/prateekgaur/Desktop/cod-rna";
-//  private static final String INPUT_FILE_TRAIN = "rcv1-v2/lyrl2004_vectors_train_5000.seq";
-  
-  private static final String INPUT_FILE_TEST = "/Users/prateekgaur/Downloads/cod-rna.t";
-//  private static final String INPUT_FILE_TEST = "rcv1-v2/lyrl2004_vectors_test_5000.seq";
-  
-  private static final String OUTPUT_TRAIN_PATH = "/Users/prateekgaur/Downloads/output-logreg-ensemble";
-  private static final String OUTPUT_TEST_PATH = "/Users/prateekgaur/Downloads/output-logreg-validation";
-  
-  public static void main(String[] args) throws Exception {
-    
-    int labelDimension = DATASET.getLabelIdByName(TARGET_POSITIVE);
-    
-    ToolRunner.run(new EnsembleJob(
-        INPUT_FILE_TRAIN, 
-        OUTPUT_TRAIN_PATH, 
-        ENSEMBLE_SIZE,
-        labelDimension,
-       /* (int)DATASET.getNumFeatures()*/11), null);
-    
-    ToolRunner.run(new EvalJob(
-        INPUT_FILE_TEST, 
-        OUTPUT_TEST_PATH,
-        OUTPUT_TRAIN_PATH,
-        labelDimension), null); 
-  }
+
+	private static final DatasetInfo DATASET = RCV1DatasetInfo.get();
+
+	private static final int ENSEMBLE_SIZE = 1;
+
+	// Currently we train a hardcoded single 1-vs-all classifier
+	private static final String TARGET_POSITIVE = "CCAT";
+
+	private static final String INPUT_FILE_TRAIN = "/Users/prateekgaur/Desktop/cod-rna";
+	//  private static final String INPUT_FILE_TRAIN = "rcv1-v2/lyrl2004_vectors_train_5000.seq";
+
+	private static final String INPUT_FILE_TEST = "/Users/prateekgaur/Downloads/cod-rna.t";
+	//  private static final String INPUT_FILE_TEST = "rcv1-v2/lyrl2004_vectors_test_5000.seq";
+
+	private static final String OUTPUT_TRAIN_PATH = "/Users/prateekgaur/Downloads/output-logreg-ensemble";
+	private static final String OUTPUT_TEST_PATH = "/Users/prateekgaur/Downloads/output-logreg-validation";
+
+	public static void main(String[] args) throws Exception {
+
+		int labelDimension = DATASET.getLabelIdByName(TARGET_POSITIVE);
+
+		ToolRunner.run(new EnsembleJob(
+				INPUT_FILE_TRAIN, 
+				OUTPUT_TRAIN_PATH, 
+				ENSEMBLE_SIZE,
+				labelDimension,
+				/* (int)DATASET.getNumFeatures()*/11), null);
+
+		ToolRunner.run(new EvalJob(
+				INPUT_FILE_TEST, 
+				OUTPUT_TEST_PATH,
+				OUTPUT_TRAIN_PATH,
+				labelDimension), null); 
+	}
 
 
 }
---
+++ ParallelLogreg/src/main/java/pactlocallogreg/EnsembleRun.java
@@ -0,0 +1,48 @@
+package pactlocallogreg;
+
+
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.Plan;
+import eu.stratosphere.client.LocalExecutor;
+import eu.stratosphere.nephele.client.JobExecutionResult;
+
+
+
+
+
+public class EnsembleRun {
+
+	private static final String INPUT_FILE_TRAIN = "file:///Users/prateekgaur/Desktop/cod-rna";
+
+	private static final String INPUT_FILE_TEST = "file:///Users/prateekgaur/Downloads/cod-rna.t";
+
+	private static final String OUTPUT_TRAIN_PATH = "file:///Users/prateekgaur/Desktop/output-pactlogreg-ensemble";
+	private static final String OUTPUT_TEST_PATH = "file:///Users/prateekgaur/Desktop/output-logreg-validation";
+
+	public static void main(String[] args) throws Exception  {
+
+
+		System.out.println("Reading input from " + INPUT_FILE_TRAIN);
+		System.out.println("Writing output to " + OUTPUT_TRAIN_PATH);
+
+
+		Plan toExecute = new EnsembleJob().getPlan( "1", INPUT_FILE_TRAIN, INPUT_FILE_TEST, OUTPUT_TRAIN_PATH, "11","1" );
+
+
+		JobExecutionResult result = LocalExecutor.execute(toExecute);
+
+		try {
+			Util.deleteAllTempFiles();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(0);
+
+	}
+
+
+
+}
